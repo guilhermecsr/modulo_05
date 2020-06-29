856425a591a157ee5aef47efa42b356cf751020f
guilhermecsr <guicsramos@gmail.com>
2020-06-29 15:08:09 -0300

    refatorando o controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index' #Dashboard
-    resources :admins, only: [:index, :edit] #admins
+    resources :admins, only: [:index, :edit, :update] #admins
   end
 
   devise_for :admins
