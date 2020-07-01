2459056184c0c2b628d203ece37de2abaf8d5c83
guilhermecsr <guicsramos@gmail.com>
2020-07-01 19:04:46 -0300

    criando as views e ajustando ao i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def translate_field(object = nil, method = nil)
+    (object && method) ? object.model.human_attribute_name(method) :
+        "Parametro nao informado"
+  end
 end
