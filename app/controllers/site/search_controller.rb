d797a95b839c0281c362f77d2e42e143a51938f5
guilhermecsr <guicsramos@gmail.com>
2020-07-06 17:52:38 -0300

    ajustes na busca

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -2,7 +2,6 @@ class Site::SearchController < SiteController
   def questions
     # params[:term]
     # @questions = Question.all
-    @questions = Question.includes(:answers)
-                         .where("lower(description) LIKE ?", "%#{params[:term].downcase}%").page(params[:page]).per(5)
+    @questions = Question.search(params[:page], params[:term])
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,10 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if:
       :all_blank, allow_destroy: true
+
+  def self.search(page, term)
+    Question.includes(:answers)
+            .where("lower(description) LIKE ?", "%#{term.downcase}%")
+            .page(page).per(5)
+  end
 end
