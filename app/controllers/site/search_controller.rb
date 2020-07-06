6442cbeac1cdd8026b84864c37546ef817b6168a
guilhermecsr <guicsramos@gmail.com>
2020-07-06 17:45:05 -0300

    busca e paginacao

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,8 @@
 class Site::SearchController < SiteController
   def questions
-    @questions = Question.all
+    # params[:term]
+    # @questions = Question.all
+    @questions = Question.includes(:answers)
+                         .where("lower(description) LIKE ?", "%#{params[:term].downcase}%").page(params[:page]).per(5)
   end
 end
