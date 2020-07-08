9fcdfbc9328ceacf2d4ae6ca94d3b7cd09d07c2f
guilhermecsr <guicsramos@gmail.com>
2020-07-08 15:00:30 -0300

    filtrando por assunto

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,9 @@
 class Site::SearchController < SiteController
   def questions
-    @questions = Question. _search_(params[:page], params[:term])
+    @questions = Question._search_(params[:page], params[:term])
+  end
+
+  def subject
+    @questions = Question._search_subject(params[:page], params[:subject_id])
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,12 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :answers, reject_if:
       :all_blank, allow_destroy: true
 
+  scope :_search_subject, ->(page, subject_id) {
+    includes(:answers, :subject)
+        .where(subject_id: subject_id)
+        .page(page)
+  }
+
   scope :_search_, ->(page, term) {
     includes(:answers)
         .where("lower(description) LIKE ?", "%#{term.downcase}%")
