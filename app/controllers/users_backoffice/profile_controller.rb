78d87c4a711f2b76e16e885851c6d6620475c8a3
guilhermecsr <guicsramos@gmail.com>
2020-07-09 15:13:19 -0300

    atualizando info do usuario com bypass para mudanca de senha

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -7,6 +7,7 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
 
   def update
     if @user.update(params_user)
+      sign_in(@user, bypass: true)
       redirect_to users_backoffice_profile_path, notice: "Usuario Atualizado com sucesso!"
     else
       render :edit
