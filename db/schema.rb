bb057e11e12f8e3d901862954e0a556667d50ddd
guilhermecsr <guicsramos@gmail.com>
2020-07-09 22:10:51 -0300

    estatisticas de usuario

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_000918) do
+ActiveRecord::Schema.define(version: 2020_07_10_001718) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -81,8 +81,8 @@ ActiveRecord::Schema.define(version: 2020_07_10_000918) do
 
   create_table "user_statistics", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "right_questions"
-    t.integer "wrong_questions"
+    t.integer "right_questions", default: 0
+    t.integer "wrong_questions", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_user_statistics_on_user_id"
