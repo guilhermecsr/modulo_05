0bc6d4e7962b1f8d56921a1ab182b4ad0a0bc3ae
guilhermecsr <guicsramos@gmail.com>
2020-07-09 21:13:06 -0300

    adicionando foto ao perfil com active storage e forcando o usuario a efetuar login

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -9,7 +9,11 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
   def update
     if @user.update(params_user)
       bypass_sign_in(@user)
-      redirect_to users_backoffice_profile_path, notice: "Usuario Atualizado com sucesso!"
+      if params_user[:user_profile_attributes][:avatar]
+        redirect_to users_backoffice_welcome_index_path, notice: "Avatar Atualizado com sucesso!"
+      else
+        redirect_to users_backoffice_profile_path, notice: "Usuario Atualizado com sucesso!"
+      end
     else
       render :edit
     end
@@ -23,7 +27,7 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
 
   def params_user
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
-                                 user_profile_attributes: [:id, :adress, :gender, :birthdate])
+                                 user_profile_attributes: [:id, :adress, :gender, :birthdate, :avatar])
   end
 
   def verify_password
--- app/controllers/users_backoffice_controller.rb
+++ app/controllers/users_backoffice_controller.rb
@@ -1,5 +1,13 @@
 class UsersBackofficeController < ApplicationController
   before_action :authenticate_user!
+  before_action :build_profile
+
   # layout 'users_backoffice'
   layout 'application'
+
+  private
+
+  def build_profile
+    current_user.build_user_profile if current_user.user_profile.blank?
+  end
 end
