f3b988ce4691578f59d3b36caf066b77e3dd16a1
guilhermecsr <guicsramos@gmail.com>
2020-07-24 17:41:36 -0300

    teste de metodo do user feito

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,13 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to be_valid }
   end
+
+  describe 'methods' do
+    let!(:user) { FactoryBot.create :user }
+    before(:each) { user.full_name }
+
+    it 'user_full_name' do
+      expect(user.full_name).to eq "#{user.first_name} #{user.last_name}"
+    end
+  end
 end
