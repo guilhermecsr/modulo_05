40eb648fdd2025c43bd45e48e00567098ff6fe61
guilhermecsr <guicsramos@gmail.com>
2020-07-31 18:25:34 -0300

    teste de feature para delete de subject nao funcional

--- spec/features/gerenciando_admins_spec.rb
+++ spec/features/gerenciando_admins_spec.rb
@@ -42,4 +42,19 @@ RSpec.feature 'Gerenciando usuários', type: :feature do
     expect(page).to have_content 'Backoffice Administrativo'
     expect(page).to have_content 'Subject Test'
   end
+
+  # scenario 'Deletando um subject', js: true do
+  #   visit '/admins/sign_in'
+  #   logando_com_admin
+  #   visit '/admins_backoffice/subjects'
+  #   visit "/admins_backoffice/subjects/#{subject.id}"
+  #   # fill_in('subject_description', with: 'Subject Test')
+  #   # click_button 'Salvar'
+  #
+  #   # expect(page).to have_current_path('/admins_backoffice/subjects')
+  #   # expect(page).to_not have_content subject.description
+  #   expect(page).to have_content 'Backoffice Administrativo'
+  #   # expect(page).to have_content 'Subject Test'
+  #   binding.pry
+  # end
 end
