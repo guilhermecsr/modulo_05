633427f89730d032fe51d35657181b2e03c8fd2e
guilhermecsr <guicsramos@gmail.com>
2020-08-12 17:55:28 -0300

    refatorando o pdf com os admins

--- app/prawn/teste_prawn.rb
+++ app/prawn/teste_prawn.rb
@@ -11,13 +11,41 @@ class TestePrawn < Prawn::Document
   end
 
   def cabecalho
-    text("TESTE")
-  end
-
-  def info_adm(adm)
-    adm.each do |admin|
-      text "#{admin.id}"
-      text admin.email
+    box(top_left, '100%', 65) do
+      cabecalho_titulo
     end
   end
+
+  def cabecalho_titulo
+    bold_font(size: 18)
+    text 'Time To Answer', align: :center
+    regular_font(size: 12)
+    text 'Sistema de Administração', align: :center
+    text 'Relatório de Admins Registrados',
+         align: :center
+    horizontal_line(-2)
+  end
+
+  # def tabela_admins
+  #
+  # end
+
+  def info_adm(adm)
+    linha = []
+    adm.each do |admin|
+      linha << ["ID: #{admin.id}",
+                "Email: #{admin.email}"]
+    end
+    table linha, :position => :center
+  end
+
+  # def info_adm(adm)
+  #   adm.each do |admin|
+  #     tabela = [
+  #         ["ID: #{admin.id}",
+  #          "Email: #{admin.email}"]
+  #     ]
+  #     table tabela, :position => :center
+  #   end
+  # end
 end
